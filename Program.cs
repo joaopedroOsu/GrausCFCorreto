d9f99385ab8921ab67651de31ec57f757ab6f091
joao <joaopedroazevedo1980@gmail.com>
2026-04-21 17:03:03 -0300

    GrausCF

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Digite a temperatura em Celsius...:");
+double temperaturac = Convert.ToDouble(Console.ReadLine());
+
+double temperaturaf = temperaturac * 1.8 + 32;
+
+Console.WriteLine($"{temperaturac}°C equivalem a {temperaturaf}°F ");
